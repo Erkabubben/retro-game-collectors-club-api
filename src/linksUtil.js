63d8040524193e5ba4ccde3e9125bd816ece13ea
Erik Lindholm <elimk06@student.lnu.se>
2022-06-13 18:31:05 +0200

    Add authenticateJWT method to LinksUtil

--- src/linksUtil.js
+++ src/linksUtil.js
@@ -7,6 +7,7 @@
  */
 
 import express from 'express'
+import jwt from 'jsonwebtoken'
 
 /**
  * Encapsulates a controller.
@@ -24,19 +25,54 @@ import express from 'express'
     const globalLinks = {
       index: ''
     }
+    // Add local links.
     for (const [key, value] of Object.entries(localLinks)) {
       linksObject[key] = {}
       linksObject[key]['href'] = fullUrl + value
     }
+    // Add global links.
     for (const [key, value] of Object.entries(globalLinks)) {
       linksObject[key] = {}
       linksObject[key]['href'] = fullUrl + value
     }
+    // Always removes trailing slash.
     for (const [key, value] of Object.entries(linksObject)) {
       if (linksObject[key]['href'].length > 0 && linksObject[key]['href'].charAt(linksObject[key]['href'].length - 1) === '/') {
         linksObject[key]['href'] = linksObject[key]['href'].substring(0, linksObject[key]['href'].length - 1);
       }
     }
     return linksObject
+  }
+
+  /**
+   * Authenticates the user by verifying the enclosed JWT.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  authenticateJWT (req) {
+    // Parses the authorization header of the request
+    const authorization = req.headers.authorization?.split(' ')
+
+    // Checks that request contains a Bearer header
+    if (authorization?.[0] !== 'Bearer') {
+      console.log('Bearer token is missing')
+      return false
+    }
+
+    try {
+      // Decodes the RSA key from base64
+      const publicKey = Buffer.from(process.env.ACCESS_TOKEN_SECRET, 'base64').toString()
+      // Verifies the JWT
+      req.jwt = jwt.verify(authorization[1], publicKey)
+      // Creates an object with user data based on the contents of the JWT
+      req.user = {
+        email: req.jwt.email,
+      }
+    } catch (error) {
+      return false
+    }
+    return true
   }
 }
--- src/routes/api/v1/router.js
+++ src/routes/api/v1/router.js
@@ -12,14 +12,25 @@ import { router as gamesRouter } from './games-router.js'
 export const router = express.Router()
 
 // Map HTTP verbs and route paths to controller actions.
-router.get('/', (req, res) => res.json({
-    message: 'Welcome to the LNU Game Collectors Club API! Please use the links to navigate.',
-    links: req.linksUtil.getLinks(req, {
-        test: 'test',
-        login: 'login',
-        register: 'register'
-    })
-}))
+router.get('/', (req, res) => {
+    let isLoggedOn = false;
+    if (req.linksUtil.authenticateJWT(req)) {
+        res.json({
+            message: 'Welcome to the LNU Game Collectors Club API! Please use the links to navigate (you are currently logged in as ' + req.user.email + '.)',
+            links: req.linksUtil.getLinks(req, {
+                myAccount: 'me',
+            })
+        })
+    } else {
+        res.json({
+            message: 'Welcome to the LNU Game Collectors Club API! Please use the links to navigate (you are currently not logged in.)',
+            links: req.linksUtil.getLinks(req, {
+                login: 'login',
+                register: 'register'
+            })
+        })
+    }
+})
 
 router.use('/games', gamesRouter)
 
